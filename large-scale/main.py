985887f57aedc3f362a22620d8ea48c9ba2158ca
RecklessRonan <380971080@qq.com>
2022-01-25 22:30:46 +0800

    Update main.py
    manual update

--- large-scale/main.py
+++ large-scale/main.py
@@ -1,13 +1,19 @@
+from torch_geometric.data import Data
+import pickle
+from torch_geometric.utils import to_networkx
+from struc_sim import graph, struc2vec
+import copy
 import argparse
 import sys
 import os
 from tqdm import tqdm
 import time
+import math
 import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch_geometric.utils import to_undirected
+from torch_geometric.utils import to_undirected, add_self_loops
 from torch_geometric.data import NeighborSampler
 from torch_scatter import scatter
 from torch_sparse import SparseTensor
@@ -20,7 +26,7 @@ from sklearn.preprocessing import normalize as sk_normalize
 from logger import Logger, SimpleLogger
 from dataset import load_nc_dataset
 from correct_smooth import double_correlation_autoscale, double_correlation_fixed
-from data_utils import evaluate_acmgcn, normalize, gen_normalized_adjs, evaluate, evaluate_mlpnorm, evaluate_acmgcn, eval_acc, eval_rocauc, to_sparse_tensor, load_fixed_splits
+from data_utils import evaluate_acmgcn, normalize, gen_normalized_adjs, evaluate, evaluate_mlpnorm, evaluate_acmgcn, evaluate_wrgat, eval_acc, eval_rocauc, to_sparse_tensor, load_fixed_splits
 from parse import parse_method, parser_add_main_args
 import faulthandler
 faulthandler.enable()
@@ -81,8 +87,10 @@ d = dataset.graph['node_feat'].shape[1]
 if not args.directed and args.dataset != 'ogbn-proteins':
     dataset.graph['edge_index'] = to_undirected(dataset.graph['edge_index'])
 
-
+#####################################################################################
 # used in GGCN
+
+
 def sparse_mx_to_torch_sparse_tensor(sparse_mx):
     """Convert a scipy sparse matrix to a torch sparse tensor."""
     sparse_mx = sparse_mx.tocoo().astype(np.float32)
@@ -127,7 +135,139 @@ def get_adj_high(adj_low):
     adj_high = -adj_low + sp.eye(adj_low.shape[0])
     return adj_high
 
+#####################################################################################
+# used in wrgat
 
+
+def build_struc_layers(G, opt1=True, opt2=True, opt3=True, until_layer=None, workers=64):
+    '''
+    Pipeline for representational learning for all nodes in a graph.
+    '''
+    if(opt3):
+        until_layer = until_layer
+    else:
+        until_layer = None
+
+    G = struc2vec.Graph(G, False, workers, untilLayer=until_layer)
+
+    if(opt1):
+        G.preprocess_neighbors_with_bfs_compact()
+    else:
+        G.preprocess_neighbors_with_bfs()
+
+    if(opt2):
+        G.create_vectors()
+        G.calc_distances(compactDegree=opt1)
+    else:
+        G.calc_distances_all_vertices(compactDegree=opt1)
+
+    G.create_distances_network()
+    G.preprocess_parameters_random_walk()
+    return
+
+
+def build_multigraph_from_layers(networkx_graph, y, x=None):
+    num_of_nodes = networkx_graph.number_of_nodes()
+
+    x_degree = torch.zeros(num_of_nodes, 1)
+    for i in range(0, num_of_nodes):
+        x_degree[i] = torch.Tensor([networkx_graph.degree(i)])
+
+    inp = open("struc_sim/pickles/distances_nets_graphs.pickle", "rb")
+    distances_nets_graphs = pickle.load(inp, encoding="bytes")
+    src = []
+    dst = []
+    edge_weight = []
+    edge_color = []
+    for layer, layergraph in distances_nets_graphs.items():
+        filename = "struc_sim/pickles/distances_nets_weights-layer-" + \
+            str(layer) + ".pickle"
+        inp = open(filename, "rb")
+        distance_nets_weights_layergraph = pickle.load(inp, encoding="bytes")
+        for node_id, nbd_ids in layergraph.items():
+            s = list(np.repeat(node_id, len(nbd_ids)))
+            d = nbd_ids
+            src += s
+            dst += d
+            edge_weight += distance_nets_weights_layergraph[node_id]
+            edge_color += list(np.repeat(layer, len(nbd_ids)))
+        assert len(src) == len(dst) == len(edge_weight) == len(edge_color)
+
+    edge_index = np.stack((np.array(src), np.array(dst)))
+    edge_weight = np.array(edge_weight)
+    edge_color = np.array(edge_color)
+
+    # print(edge_index.shape)
+    # print(edge_weight.shape)
+    if x is None:
+        data = Data(x=x_degree, edge_index=torch.LongTensor(edge_index), edge_weight=torch.FloatTensor(edge_weight),
+                    edge_color=torch.LongTensor(edge_color), y=y)
+    else:
+        data = Data(x=x, x_degree=x_degree, edge_index=torch.LongTensor(edge_index),
+                    edge_weight=torch.FloatTensor(edge_weight),
+                    edge_color=torch.LongTensor(edge_color), y=y)
+
+    return data
+
+
+def build_pyg_struc_multigraph(pyg_data):
+    # print("Start build_pyg_struc_multigraph")
+    start_time = time.time()
+    # print(pyg_data)
+    G = graph.from_pyg(pyg_data)
+    # print('Before G', G)
+    networkx_graph = to_networkx(pyg_data)
+    # print('networkx_graph', networkx_graph)
+    print("Done converting to networkx")
+    build_struc_layers(G)
+    # print('After G', G)
+    print("Done building layers")
+    data = build_multigraph_from_layers(networkx_graph, pyg_data.y, pyg_data.x)
+    # print(data)
+    if hasattr(pyg_data, 'train_mask'):
+        data.train_mask = pyg_data.train_mask
+        data.val_mask = pyg_data.val_mask
+        data.test_mask = pyg_data.test_mask
+    time_cost = time.time() - start_time
+    print("build_pyg_struc_multigraph cost: ", time_cost)
+    return data
+
+
+def filter_rels(data, r):
+    data = copy.deepcopy(data)
+    mask = data.edge_color <= r
+    data.edge_index = data.edge_index[:, mask]
+    data.edge_weight = data.edge_weight[mask]
+    data.edge_color = data.edge_color[mask]
+    return data
+
+
+def structure_edge_weight_threshold(data, threshold):
+    data = copy.deepcopy(data)
+    mask = data.edge_weight > threshold
+    data.edge_weight = data.edge_weight[mask]
+    data.edge_index = data.edge_index[:, mask]
+    data.edge_color = data.edge_color[mask]
+    return data
+
+
+def add_original_graph(og_data, st_data, weight=1.0):
+    st_data = copy.deepcopy(st_data)
+    e_i = torch.cat((og_data.edge_index, st_data.edge_index), dim=1)
+    st_data.edge_color = st_data.edge_color + 1
+    e_c = torch.cat((torch.zeros(
+        og_data.edge_index.shape[1], dtype=torch.long), st_data.edge_color), dim=0)
+    e_w = torch.cat((torch.ones(
+        og_data.edge_index.shape[1], dtype=torch.float)*weight, st_data.edge_weight), dim=0)
+    st_data.edge_index = e_i
+    st_data.edge_color = e_c
+    st_data.edge_weight = e_w
+    return st_data
+
+#####################################################################################
+
+
+num_relations = None
 if args.method == 'mlpnorm':
     x = dataset.graph['node_feat']
     edge_index = dataset.graph['edge_index']
@@ -192,6 +332,47 @@ elif args.method == 'acmgcn':
     x = x.to(device)
     adj_low = adj_low.to(device)
     adj_high = adj_high.to(device)
+elif args.method == 'wrgat':
+    st_data_pt = 'wrgnn_features/' + args.dataset + '_st.pt'
+    og_data_pt = 'wrgnn_features/' + args.dataset + '_og.pt'
+    if os.path.exists(st_data_pt) and os.path.exists(og_data_pt):
+        st_data = torch.load(st_data_pt)
+        og_data = torch.load(og_data_pt)
+    else:
+        if args.dataset in ['arxiv-year']:
+            print('before edge_index', dataset.graph['edge_index'])
+            dataset.graph['edge_index'] = add_self_loops(
+                dataset.graph['edge_index'], num_nodes=dataset.graph['num_nodes'])[0]
+            print('after edge_index', dataset.graph['edge_index'])
+
+        og_data = Data(x=dataset.graph['node_feat'],
+                       edge_index=dataset.graph['edge_index'], y=dataset.label, num_nodes=dataset.graph['num_nodes'])
+        st_data = build_pyg_struc_multigraph(og_data)
+        torch.save(st_data, st_data_pt)
+        torch.save(og_data, og_data_pt)
+    
+
+    if args.filter_structure_relation:
+        print("Filtering structure relations with number %d" %
+              args.filter_structure_relation_number)
+        st_data = filter_rels(st_data, args.filter_structure_relation_number)
+
+    st_data = structure_edge_weight_threshold(st_data, args.st_thres)
+
+    if args.original_edges:
+        print("Adding original graph edges with weight %f" %
+              args.original_edges_weight)
+        data = add_original_graph(
+            og_data, st_data, weight=args.original_edges_weight)
+    else:
+        data = st_data
+    print('og_data', og_data)
+    print('st_data', st_data)
+    num_relations = len(data.edge_color.unique())
+    x = data.x.to(device)
+    edge_index = data.edge_index.to(device)
+    edge_weight = data.edge_weight.to(device)
+    edge_color = data.edge_color.to(device)
 else:
     dataset.graph['edge_index'], dataset.graph['node_feat'] = \
         dataset.graph['edge_index'].to(
@@ -204,7 +385,7 @@ print(f"num nodes {n} | num classes {c} | num node feats {d}")
 # sys.exit()
 ### Load method ###
 
-model = parse_method(args, dataset, n, c, d, device)
+model = parse_method(args, dataset, n, c, d, device, num_relations)
 # print('model', next(model.parameters()).device)
 
 
@@ -255,7 +436,6 @@ if args.method == 'lp':
 
 model.train()
 print('MODEL:', model)
-
 ### Training loop ###
 last_time = time.time()
 
@@ -303,9 +483,11 @@ for run in range(args.runs):
                 out = model(x, adj)
             elif args.method == 'acmgcn':
                 out = model(x, adj_low, adj_high)
+            elif args.method == 'wrgat':
+                out = model(x, edge_index, edge_weight, edge_color)
             else:
                 out = model(dataset)
-            #loss = criterion(out[train_idx], dataset.label.squeeze(1)[train_idx].type_as(out))
+            # loss = criterion(out[train_idx], dataset.label.squeeze(1)[train_idx].type_as(out))
 
             # print('out', out.shape)
             if args.rocauc or args.dataset in ('yelp-chi', 'twitch-e', 'ogbn-proteins', 'genius'):
@@ -352,6 +534,9 @@ for run in range(args.runs):
         elif args.method == 'acmgcn':
             result = evaluate_acmgcn(model, x, adj_low, adj_high, dataset, split_idx, eval_func,
                                      sampling=args.sampling, subgraph_loader=subgraph_loader)
+        elif args.method == 'wrgat':
+            result = evaluate_wrgat(model, x, edge_index, edge_weight, edge_color, dataset, split_idx, eval_func,
+                                    sampling=args.sampling, subgraph_loader=subgraph_loader)
         else:
             result = evaluate(model, dataset, split_idx, eval_func,
                               sampling=args.sampling, subgraph_loader=subgraph_loader)
